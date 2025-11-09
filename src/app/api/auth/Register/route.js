bb3a10a96628a37a2312d76b359957da78459299
trailmix-ship-it <samuelregan11@gmail.com>
2025-11-09 19:25:51 +0000

    login prototype - just nextjs, reactjs, etc. v2

--- src/app/api/auth/Register/route.js
+++ src/app/api/auth/Register/route.js
@@ -12,7 +12,6 @@
 */
 
 // taken from authentication beta docs, nextJS on the 07/11/2025: https://nextjs.org/docs/app/guides/authentication
-/*
 import bcrypt from "bcrypt";
 import sql from "@/lib/db";
 import jwt from 'jsonwebtoken';
@@ -20,8 +19,10 @@ import {cookies} from "next/headers";
 import {NextResponse} from "next/server";
 import {redirect} from "next/navigation";
 
-
 export async function POST(request) {
+    //error handling sourced from nextjs beta docs, accessed 09/11/2025: https://nextjs.org/docs/app/api-reference/file-conventions/route#error-handling
+    // 1. Validate form fields
+    // based loosely off geeksforgeeks validation tutorial, accessed 07/11/25: https://www.geeksforgeeks.org/javascript/form-validation-using-javascript/
     const body = await request.json();
     const username = body.username;
     const userPwd = body.password;
@@ -38,41 +39,52 @@ export async function POST(request) {
         return isValid;
     }
 
-    // 2. Prepare data for insertion into database
-    const validatedFields = {username, password: userPwd};
-    const {username: name, password} = validatedFields;
-    // e.g. Hash the user's password before storing it
-    const hashedPassword = await bcrypt.hash(password, 10);
+    //6. error handling
+    try {
+        validateFields();
+        if (isValid) {
+            // 2. Prepare data for insertion into database
+            const validatedFields = {username, password: userPwd};
+            const {username: name, password} = validatedFields;
+            // e.g. Hash the user's password before storing it
+            const hashedPassword = await bcrypt.hash(password, 10);
 
-    // 3. Insert the user into the database or call a Library API
-    const data = await sql`
-        INSERT INTO login ("userName", password)
-        VALUES (${name}, ${hashedPassword})
-        RETURNING "userID"`;
+            // 3. Insert the user into the database or call a Library API
+            const data = await sql`
+                    INSERT INTO login ("userName", password)
+                    VALUES (${name}, ${hashedPassword})
+                    RETURNING "userID"`;
 
-    const user = data[0] //this contains the returned row from INSERT - used for jwt payload
+            const user = data[0] //this contains the returned row from INSERT - used for jwt payload
 
-    if (!user) {
-        return NextResponse.json({error: 'An error occurred while creating your account.'});
-    }
-}
-*/
-/*
-    function hash(req, res, next) {
-        const hashedPassword = await bcrypt.hash(pwd, 13);
-
-        const result = await sql("SELECT * FROM users WHERE user_id = ${user}");
-        if (result.rows.length > 0) {
-            const match = await bcrypt.compare(pwd, result.rows[0].password);
-            if (match) {
-
+            if (!user) {
+                return NextResponse.json({error: 'An error occurred while creating your account.'});
             }
+
+            // 4. Create user session (generating/returning JWT)
+            // sourced from jsonwebtoken library documentation: https://github.com/auth0/node-jsonwebtoken#usage (accessed 09/11/25)
+            const token = jwt.sign({userID: user.userID, username: name},
+                process.env.JWT_SECRET,
+                {expiresIn: '7d'}
+            );
+
+            const expires = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7); //expires in a week from when it was made
+
+            cookies().set('session', token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                expires: expires,
+                sameSite: 'lax',
+                path: '/' //this just means the cookie can be accessed on all routes
+            });
+
+            // 5. Redirect user
+            redirect('/'); //sends them to the home page
+        } else if (!isValid) {
+            return NextResponse.json({error: 'missing fields'});
         }
-
-    }
-
-
-    function JWTCall(req, res, next) {
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({error: 'internal server error'});
     }
 }
-*/
