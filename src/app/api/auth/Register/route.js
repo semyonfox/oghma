a653b8eb2875aa6c33986db24061d35047b293a8
trailmix-ship-it <samuelregan11@gmail.com>
2025-11-09 18:42:13 +0000

    login prototype - just nextjs, reactjs, etc. v2

--- src/app/api/auth/Register/route.js
+++ src/app/api/auth/Register/route.js
@@ -11,22 +11,68 @@
 * handle errors (validation failures/db errors/duplicate user/etc.)
 */
 
-function validateUser(req, res, next) {}
+// taken from authentication beta docs, nextJS on the 07/11/2025: https://nextjs.org/docs/app/guides/authentication
+import bcrypt from "bcrypt";
+import sql from "@/lib/db";
+import jwt from 'jsonwebtoken';
+import {cookies} from "next/headers";
+import {NextResponse} from "next/server";
+import {redirect} from "next/navigation";
 
-function serverQuery(req, res, next) {}
 
-function hash(req, res, next) {
-    const hashedPassword = await bcrypt.hash(pwd,13);
+export async function POST(request) {
+    const body = await request.json();
+    const username = body.username;
+    const userPwd = body.password;
 
-    const result = await sql("SELECT * FROM users WHERE user_id = ${user}");
-    if (result.rows.length > 0) {
-        const match = await bcrypt.compare(pwd, result.rows[0].password);
-        if (match) {
+    let isValid = true;
+    function validateFields() {
 
+        if (username == null || username == "") {
+            isValid = false;
         }
+        if (userPwd == null || userPwd == "") {
+            isValid = false;
+        }
+        return isValid;
     }
 
+    // 2. Prepare data for insertion into database
+    const validatedFields = {username, password: userPwd};
+    const {username: name, password} = validatedFields;
+    // e.g. Hash the user's password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 3. Insert the user into the database or call a Library API
+    const data = await sql`
+        INSERT INTO login ("userName", password)
+        VALUES (${name}, ${hashedPassword})
+        RETURNING "userID"`;
+
+    const user = data[0] //this contains the returned row from INSERT - used for jwt payload
+
+    if (!user) {
+        return NextResponse.json({error: 'An error occurred while creating your account.'});
+    }
 }
 
-function JWTCall(req, res, next) {}
+/*
+    function hash(req, res, next) {
+        const hashedPassword = await bcrypt.hash(pwd, 13);
+
+        const result = await sql("SELECT * FROM users WHERE user_id = ${user}");
+        if (result.rows.length > 0) {
+            const match = await bcrypt.compare(pwd, result.rows[0].password);
+            if (match) {
+
+            }
+        }
+
+    }
+
+ */
+
+    function JWTCall(req, res, next) {
+    }
+}
 
