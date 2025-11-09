48fc93460d315b523c07daa84504a4bb6dd9a2f7
semyonfox <semyon.fox@gmail.com>
2025-11-09 23:45:43 +0000

    feat: implement registration and update login functionality with fetch API

--- src/app/api/axios.js
+++
@@ -1,8 +0,0 @@
-/*
-* Taken from Dave Gray's "React Login Authentication with JWT Access, Refresh Tokens, Cookies and Axios"
-* youtube tutorial for creating a private axios instance*/
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'http://localhost:3000',
-})
