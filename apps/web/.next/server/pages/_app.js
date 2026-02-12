1bd63fc6baa990a41e2637c9346500373b217692
Semyon Fox <semyon.fox@gmail.com>
2026-02-12 01:09:41 +0000

    fix: remove obsolete root postcss.config.js causing ES module error

--- apps/web/.next/server/pages/_app.js
+++ apps/web/.next/server/pages/_app.js
@@ -1,4 +1,4 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__14c62f8b._.js")
-R.m(72256)
-module.exports=R.m(72256).exports
+R.c("server/chunks/ssr/[root-of-the-server]__ba40133b._.js")
+R.m(60552)
+module.exports=R.m(60552).exports
