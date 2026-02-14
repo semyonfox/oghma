74e454e462a2b6a10d44f5f21d24e31d9a5bd1dd
semyonfox <semyon.fox@gmail.com>
2026-02-14 18:23:45 +0000

    chore: remove obsolete apps/web directory - monorepo structure not needed - all components now in correct src/ location - clean up duplicate directory structure

--- apps/web/src/lib/notes/hooks/use-i18n.tsx
+++
@@ -1,8 +0,0 @@
-// extracted from Notea (MIT License)
-import { useContext } from 'react';
-import { I18nContext } from '../utils/i18n-provider';
-
-export default function useI18n() {
-    const i18n = useContext(I18nContext);
-    return i18n;
-}
--- apps/web/src/lib/storage/index.ts
+++
@@ -1,8 +0,0 @@
-// S3 storage provider for socsboard
-// extracted from Notea (MIT License)
-
-export { StoreProvider, StoreProviderConfig, ObjectOptions } from './base';
-export { StoreS3, S3Config } from './s3';
-export { streamToBuffer } from './utils';
-export { toBuffer, toStr, tryJSON, strCompress, strDecompress } from './str';
-export { createLogger, Logger } from './logger';
