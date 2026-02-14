4af05db7bd29299725d1e047bdb55ce11dda4498
semyonfox <semyon.fox@gmail.com>
2026-02-14 18:23:45 +0000

    chore: remove obsolete apps/web directory
    - monorepo structure not needed
    - all components now in correct src/ location
    - clean up duplicate directory structure

--- apps/web/src/lib/notes/utils/markdown.ts
+++
@@ -1,8 +0,0 @@
-// extracted from Notea (MIT License)
-// original: libs/web/utils/markdown.ts
-
-import rm from 'remove-markdown';
-
-export const removeMarkdown = (markdown?: string) => {
-  return rm(markdown).replace(/\\/g, '');
-};
