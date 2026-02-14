82499daf6cfa33c836c2cb45175ba6c029767006
semyonfox <semyon.fox@gmail.com>
2026-02-14 14:26:28 +0000

    chore: remove build artifacts and lock files from git tracking
    - remove .next/ build cache from tracked files
    - remove node_modules/ from tracked files
    - remove pnpm-lock.yaml and pnpm-workspace.yaml
    - update .gitignore to reflect proper ignore rules
    - these files should never have been committed; rely on .gitignore
    
    LOCAL COMMIT ONLY - DO NOT PUSH

--- apps/web/.next/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__ba40133b._.js")
-R.m(60552)
-module.exports=R.m(60552).exports
