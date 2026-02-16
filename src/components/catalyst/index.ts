47b098b99c10b5be381a04cd12813def2481385b
semyonfox <semyon.fox@gmail.com>
2026-02-16 21:25:10 +0000

    feat: add Catalyst UI component library - Copy 13+ core Catalyst components (button, input, alert, fieldset, checkbox, link, heading, text, sidebar, navbar, avatar, dropdown, badge, auth-layout, etc.) - Install motion/react dependency for animations - Components integrate with existing Headless UI dependencies - Provides modern, accessible UI foundation for redesign

---
+++ src/components/catalyst/index.ts
@@ -0,0 +1,16 @@
+// Core Catalyst UI Components
+// All components are re-exported from this index for easier imports
+
+export * from './button'
+export * from './input'
+export * from './alert'
+export * from './fieldset'
+export * from './checkbox'
+export * from './link'
+export * from './heading'
+export * from './text'
+export * from './sidebar'
+export * from './navbar'
+export * from './avatar'
+export * from './dropdown'
+export * from './badge'
