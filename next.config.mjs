f0f8b2881c3fd20a8c6f949540f6d86230ecfd0c
Semyon Fox <semyon.fox@gmail.com>
2026-03-05 13:28:00 +0000

    fix: remove serverExternalPackages that caused module resolution issues - removed serverExternalPackages config that was breaking postgres import - reverts to default next.js bundling which handles postgres correctly - should fix 'n.default is not a function' error on Amplify

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    serverExternalPackages: ['postgres', 'bcryptjs', 'jsonwebtoken'],
-};
+const nextConfig = {};
 
 export default nextConfig;
