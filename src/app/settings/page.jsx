fa2e1066f0c10bbd733986d10dce0be09ecf43e4
semyon <semyon.fox@gmail.com>
2026-06-21 01:56:57 +0100

    fix: import React on settings page

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import {
